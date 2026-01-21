c824bc3393e8f55ecfaf8ecb37e7204d37486e87
sakshamman <ssaksham22@tbc.edu.npgit config --global user.name sakshammangit config --global user.email ssaksham22@tbc.edu.np>
2026-01-21 22:33:18 +0545

    filled form added if else option for mandatory fields and also added selenium support for options section

--- src/test/java/gettingStarted/SeleniumTest.java
+++ src/test/java/gettingStarted/SeleniumTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SeleniumTest {
 
@@ -39,6 +40,30 @@ public class SeleniumTest {
 
         //click login button
         driver.findElement(By.cssSelector(".btn")).click();
+
+        driver.findElement(By.linkText("Employee List")).click();
+
+        driver.findElement(By.linkText("Create New")).click();
+
+        driver.findElement(By.name("Name")).sendKeys("AutoUser1");
+
+        driver.findElement(By.id("DurationWorked")).sendKeys("40");
+
+        Select selectGrade = new Select(driver.findElement(By.id("Grade")));
+        selectGrade.selectByVisibleText("Middle");
+
+        driver.findElement(By.name("Email")).sendKeys("AutoUser@gmail.com");
+
+        driver.findElement(By.cssSelector(".btn")).click();
+
+        //if mandatory field error for salary appears, then enter salary
+
+        if(driver.findElement(By.xpath("//span[text()='The Salary field is required.']")).isDisplayed()){
+            driver.findElement(By.name("Salary")).sendKeys("10000");
+            driver.findElement(By.cssSelector(".btn")).click();
+        } else {
+            //do nothing
+        }
 
 
     }
